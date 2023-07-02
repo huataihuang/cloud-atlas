006b51005a233a4b765fe0a7da477e693e5ecf0f
huataihuang <huataihuang@gmail.com>
2023-07-02 23:53:00 +0800

    gluserfs init disk, xfs kernel incompatible

--- source/gluster/deploy/centos/centos_gluster_init/parted_xfs.sh
+++ source/gluster/deploy/centos/centos_gluster_init/parted_xfs.sh
@@ -5,6 +5,9 @@ for i in {0..11};do
     parted -s -a optimal /dev/nvme${i}n1 name 1 gluster_brick${i}
     sleep 1
     mkfs.xfs -f -i size=512 /dev/nvme${i}n1p1
+    # 如果内核版本低于3.16，则只支持 xfs superblock v4
+    # mkfs.xfs (xfsprogs 3.2.4 )默认格式化是 superblock v5，需要增加 -m crc=0,finobt=0 来格式化成 xfs superblock v4:
+    # mkfs.xfs -f -i size=512 -m crc=0,finobt=0 /dev/nvme${i}n1p1
     fstab_line=`grep "/dev/nvme${i}n1p1" /etc/fstab`
     if [ ! -n "$fstab_line"  ];then echo "/dev/nvme${i}n1p1 /data/brick${i} xfs rw,inode64,noatime,nouuid 1 2" >> /etc/fstab;fi
     mount /data/brick${i}
