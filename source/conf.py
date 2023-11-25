0d6f85884a91baec87b351a4936853942bc1e979
huataihuang <huataihuang@gmail.com>
2023-11-25 13:49:31 +0800

    remove language zh_CN to speed sphinx doc build

--- source/conf.py
+++ source/conf.py
@@ -67,7 +67,7 @@ master_doc = 'index'
 # This is also used if you do content translation via gettext catalogs.
 # Usually you set "language" from the command line for these cases.
 #language = None
-language = 'zh_CN'
+#language = 'zh_CN'
 #html_search_language = 'zh'
 
 # List of patterns, relative to source directory, that match files and
